d514633775c362ee4c143bffd6543c76c56e9d8b
PP-Pixel-Party <119059416+PP-Pixel-Party@users.noreply.github.com>
2022-11-29 19:31:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,126 @@
-
+import os
+{
+    "env": {
+        "BOT_TOKEN": {
+            "description": "Your bot token.",
+            "value": "5385553998:AAH7ID5UG_pixxocYWZVkDcooKLdodkZDX0",
+            "required": true
+        },
+        "API_ID": {
+            "description": "Get this value from telegram.org",
+            "value": "18442173",
+            "required": true
+        },
+        "API_HASH": {
+            "description": "Get this value from telegram.org",
+            "value": "115d8326136c0fe629f02082500a044d",
+            "required": true
+        },
+        "CHANNELS": {
+            "description": "Username or ID of channel or group. Separate multiple IDs by space",
+            "value": "-1001660384398 -1001167665304",
+            "required": true
+        },
+        "COLLECTION_NAME": {
+            "description": "Name of the collections. Defaults to Telegram_files. If you are using the same database, then use different collection name for each bot",
+            "value": "Telegram_files",
+            "required": false
+        },
+        "DATABASE_NAME": {
+            "description": "Name of the database in https://www.mongodb.com/. For more help watch this https://youtu.be/mD9veNL7KoE",
+            "value": "LuciferMoringstar_Robot",
+            "required": false
+        },
+        "DATABASE_URL": {
+            "description": "Name of the database in https://www.mongodb.com/. For more help watch this https://youtu.be/mD9veNL7KoE",
+            "value": "mongodb+srv://pp-filter-bot:pp-filter-bot@cluster0.kogpi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+            "required": true
+        },
+        "FORCE_SUB": {
+            "description": "ID of channel. Without subscribing this channel users cannot use Autofilter",
+            "value": "-1001323263422",
+            "required": false
+        },
+        "GITHUB_USERNAME": {
+            "description": "Enter Your Github Username Pr0fess0r-99",
+            "value": "PP-Pixel-Party",
+            "required": true
+        },
+        "LOG_CHANNEL": {
+            "description": "A channel to log the activities of bot. Make sure bot is an admin in the channel",
+            "value": " ",
+            "required": true
+        },
+        "REPO_USERNAME": {
+            "description": "Enter Repository name Midukki-RoBoT",
+            "value": "Dean-Winchester",
+            "required": false
+        },
+        "API_WEB_URL": {
+            "description": "Ads Web Url eg: htts://github.com",
+            "value": "",
+            "required": false
+        },
+        "API_WEB_API": {
+            "description": "Ads Web Api",
+            "value": "",
+            "required": false
+        },
+        "DONATION_LINK": {
+            "description": "enter your gpay, pypal etc.. link",
+            "value": "",
+            "required": false
+        },
+        "AUTH_USERS": {
+            "description": "Username or ID of users to give access of inline search & pm filter. Separate multiple users by space. Leave it empty if you don't want to restrict bot usage.",
+            "value": "",
+            "required": false
+        },
+        "AUTH_GROUPS": {
+            "description": "ID of groups which bot should work as autofilter, bot can only work in thease groups. If not given , bot can be used in any group.",
+            "value": "",
+            "required": false
+        },
+        "BOT_PICS": {
+            "description": "Telegraph links of images to show in start message.( Multiple images can be used separated by space )",
+            "value": "https://telegra.ph/file/83b9ed72729aa44b59366.jpg",
+            "required": false
+        },
+        "STRING_SESSION": {
+            "description": "User bot string session.",
+            "value": "",
+            "required": false
+        },
+        "DEFAULT": {
+            "description": "Dont Change this value.",
+            "value": "False",
+            "required": false
+        },
+        "FILTER_RESULTS": {
+            "description": "AutoFilter result button count.",
+            "value": "10",
+            "required": false
+        },
+        "LOADING_MODE": {
+            "description": "Loading feature ON/OFF. this features worked in callback",
+            "value": "ON",
+            "required": false
+        },
+        "LOADING_A": {
+            "description": "Loading First Symbol ⚪️.",
+            "value": "⚪️",
+            "required": false
+        },
+        "LOADING_B": {
+            "description": "Loading Second Symbol ⚫️.",
+            "value": "⚫️",
+            "required": false
+        },
+        "SUPPORT_CHAT": {
+            "description": "Your Support chat url or Your Link [with https:// or https://t.me/] Don't use @",
+            "value": "https://t.me/PixelPartyUpdates",
+            "required": false
+        }
+      
+      
+}
